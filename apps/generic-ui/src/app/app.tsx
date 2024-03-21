6cddce0bfea9b0463405735eafcf57fd4355e1b0
Steve Ernstberger <steve32285@gmail.com>
2024-03-21 08:57:15 -0400

    added react aria and tw

--- apps/generic-ui/src/app/app.tsx
+++ apps/generic-ui/src/app/app.tsx
@@ -1,9 +1,22 @@
+import { Heading, Text } from 'react-aria-components';
+import { Button } from './Button';
+
 export function App() {
   return (
     <div>
-      <div>hi</div>
+      <Billboard>
+        <Button variant="destructive" onPress={() => alert('Hello world!')}>
+          Save
+        </Button>
+      </Billboard>
+      <Heading level={1}>Hello, world!</Heading>
+      <Text>Some text</Text>
     </div>
   );
 }
+
+const Billboard = ({ children }: any) => {
+  return <div className="flex p-8 bg-slate-500">{children}</div>;
+};
 
 export default App;
