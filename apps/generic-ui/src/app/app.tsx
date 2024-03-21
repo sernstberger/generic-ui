7b2158924a022d54c479ebf1a9b519fc2094adf3
Steve Ernstberger <steve32285@gmail.com>
2024-03-21 16:48:00 -0400

    cleanup sheet

--- apps/generic-ui/src/app/app.tsx
+++ apps/generic-ui/src/app/app.tsx
@@ -9,11 +9,90 @@ export function App() {
           Save
         </Button>
       </Billboard>
-      <Sheet>aldskfjadslfjdsalfadslfj</Sheet>
-      <Sheet color="destructive">aldskfjadslfjdsalfadslfj</Sheet>
-      <Sheet color="success">aldskfjadslfjdsalfadslfj</Sheet>
-      <Sheet color="secondary">aldskfjadslfjdsalfadslfj</Sheet>
-      <Heading level={1}>Hello, world!</Heading>
+
+      <Heading level={1}>Plain</Heading>
+      <Sheet color="primary" variant="plain">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="neutral" variant="plain">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="destructive" variant="plain">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="success" variant="plain">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="warning" variant="plain">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+
+      <br />
+
+      <Heading level={1}>Outlined</Heading>
+      <Sheet color="primary" variant="outlined">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="neutral" variant="outlined">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="destructive" variant="outlined">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="success" variant="outlined">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="warning" variant="outlined">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+
+      <br />
+
+      <Heading level={1}>Soft</Heading>
+      <Sheet color="primary" variant="soft">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="neutral" variant="soft">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="destructive" variant="soft">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="success" variant="soft">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="warning" variant="soft">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+
+      <br />
+
+      <Heading level={1}>Solid</Heading>
+      <Sheet color="primary" variant="solid">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="neutral" variant="solid">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="destructive" variant="solid">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="success" variant="solid">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+      <Sheet color="warning" variant="solid">
+        aldskfjadslfjdsalfadslfj
+      </Sheet>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <hr />
+
       <Text>Some text</Text>
     </div>
   );
--- libs/components/src/lib/Sheet.tsx
+++ libs/components/src/lib/Sheet.tsx
@@ -8,32 +8,106 @@ import { tv } from 'tailwind-variants';
 
 export interface ButtonProps extends RACButtonProps {
   variant?: 'plain' | 'outlined' | 'soft' | 'solid';
-  color?: 'primary' | 'secondary' | 'destructive' | 'success' | 'warning';
+  color?: 'primary' | 'neutral' | 'destructive' | 'success' | 'warning';
+}
+
+function createPlainVariant(
+  color: 'primary' | 'neutral' | 'destructive' | 'success' | 'warning',
+  bgColor: any,
+  textColor: any
+): any {
+  return {
+    variant: 'plain',
+    color: color,
+    class: `${bgColor} ${textColor}`,
+  };
+}
+
+function createOutlinedVariant(
+  color: 'primary' | 'neutral' | 'destructive' | 'success' | 'warning',
+  borderColor: any,
+  textColor: any
+): any {
+  return {
+    variant: 'outlined',
+    color: color,
+    class: `${borderColor} ${textColor}`,
+  };
+}
+
+function createSoftVariant(
+  color: 'primary' | 'neutral' | 'destructive' | 'success' | 'warning',
+  bgColor: any,
+  textColor: any
+): any {
+  return {
+    variant: 'soft',
+    color: color,
+    class: `${bgColor} ${textColor}`,
+  };
+}
+
+function createSolidVariant(
+  color: 'primary' | 'neutral' | 'destructive' | 'success' | 'warning',
+  bgColor: any,
+  textColor: any
+): any {
+  return {
+    variant: 'solid',
+    color: color,
+    class: `${bgColor} ${textColor}`,
+  };
 }
 
 const sheet = tv({
-  // extend: focusRing,
-  base: 'px-5 py-2 text-sm text-center transition border border-black/10 dark:border-white/10 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.1)] dark:shadow-none cursor-default',
+  base: 'p-2',
+  // base: 'px-5 py-2 text-sm text-center transition border border-black/10 dark:border-white/10 shadow-[inset_0_1px_0_0_rgba(255,255,255,0.1)] dark:shadow-none cursor-default',
   variants: {
     color: {
-      primary: 'bg-blue-600 hover:bg-blue-700 pressed:bg-blue-800 text-white',
-      secondary:
-        'bg-gray-100 hover:bg-gray-200 pressed:bg-gray-300 text-gray-800 dark:bg-zinc-600 dark:hover:bg-zinc-500 dark:pressed:bg-zinc-400 dark:text-zinc-100',
-      destructive: 'bg-red-700 hover:bg-red-800 pressed:bg-red-900 text-white',
-      success:
-        'bg-green-700 hover:bg-green-800 pressed:bg-green-900 text-white',
-      warning:
-        'bg-yellow-700 hover:bg-yellow-800 pressed:bg-yellow-900 text-white',
-      // icon: 'border-0 p-1 flex items-center justify-center text-gray-600 hover:bg-black/[5%] pressed:bg-black/10 dark:text-zinc-400 dark:hover:bg-white/10 dark:pressed:bg-white/20 disabled:bg-transparent',
+      primary: '',
+      neutral: '',
+      destructive: '',
+      success: '',
+      warning: '',
     },
-    isDisabled: {
-      true: 'bg-gray-100 dark:bg-zinc-800 text-gray-300 dark:text-zinc-600 forced-colors:text-[GrayText] border-black/5 dark:border-white/5',
+    variant: {
+      // plain: 'bg-transparent',
+      plain: '',
+      outlined: 'border',
+      soft: '',
+      solid: '',
     },
   },
   defaultVariants: {
     variant: 'plain',
     color: 'primary',
   },
+
+  compoundVariants: [
+    createPlainVariant('primary', 'bg-blue-50', 'text-blue-600'),
+    createPlainVariant('neutral', 'bg-gray-50', 'text-gray-600'),
+    createPlainVariant('destructive', 'bg-red-50', 'text-red-600'),
+    createPlainVariant('success', 'bg-green-50', 'text-green-600'),
+    createPlainVariant('warning', 'bg-yellow-50', 'text-yellow-600'),
+
+    createOutlinedVariant('primary', 'border-blue-600', 'text-blue-600'),
+    createOutlinedVariant('neutral', 'border-gray-600', 'text-gray-600'),
+    createOutlinedVariant('destructive', 'border-red-600', 'text-red-600'),
+    createOutlinedVariant('success', 'border-green-600', 'text-green-600'),
+    createOutlinedVariant('warning', 'border-yellow-600', 'text-yellow-600'),
+
+    createSoftVariant('primary', 'bg-blue-100', 'text-blue-900'),
+    createSoftVariant('neutral', 'bg-gray-100', 'text-gray-900'),
+    createSoftVariant('destructive', 'bg-red-100', 'text-red-900'),
+    createSoftVariant('success', 'bg-green-100', 'text-green-900'),
+    createSoftVariant('warning', 'bg-yellow-100', 'text-yellow-900'),
+
+    createSolidVariant('primary', 'bg-blue-600', 'text-blue-900'),
+    createSolidVariant('neutral', 'bg-gray-600', 'text-gray-900'),
+    createSolidVariant('destructive', 'bg-red-600', 'text-red-900'),
+    createSolidVariant('success', 'bg-green-600', 'text-green-900'),
+    createSolidVariant('warning', 'bg-yellow-600', 'text-yellow-900'),
+  ],
 });
 
 export function Sheet(props: ButtonProps) {
@@ -43,7 +117,7 @@ export function Sheet(props: ButtonProps) {
       className={composeRenderProps(props.className, (className, renderProps) =>
         sheet({
           ...renderProps,
-          // variant: props.variant,
+          variant: props.variant,
           color: props.color,
           className,
         })
