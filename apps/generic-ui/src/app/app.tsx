621f033820505bdf062e16723320809e9963661b
Steve Ernstberger <steve32285@gmail.com>
2024-03-28 08:57:10 -0400

    layout component

--- apps/generic-ui/src/app/app.tsx
+++ apps/generic-ui/src/app/app.tsx
@@ -11,60 +11,48 @@ export function App() {
 
   return (
     <Provider theme={defaultTheme} colorScheme="light" router={{ navigate }}>
-      <Grid
-        areas={['header  header', 'sidebar content', 'footer  footer']}
-        // columns={['1fr', '3fr']}
-        columns={[240, '3fr']}
-        rows={['size-1000', 'auto', 'size-1000']}
-        height="100vh"
-        gap="size-100"
-      >
-        <View backgroundColor="celery-600" gridArea="header" />
-        <View backgroundColor="blue-600" gridArea="sidebar" />
-        <View gridArea="content">
-          <Billboard>
-            <div>Avatar</div>
-            <Button startIcon={<Heart />}>Save</Button>
-          </Billboard>
+      <Layout>
+        <Billboard>
+          <div>Avatar</div>
+          <Button startIcon={<Heart />}>Save</Button>
+        </Billboard>
+        <div>
+          <Heading level={1}>Starbucks</Heading>
+          <SearchField />
+        </div>
+        <div>
+          <Button>Group order</Button>
           <div>
-            <Heading level={1}>Starbucks</Heading>
-            <SearchField />
-          </div>
-          <div>
-            <Button>Group order</Button>
+            <div>$0.00</div>
             <div>
-              <div>$0.00</div>
-              <div>
-                Delivery fee
-                <span>tooltip</span>
-              </div>
-            </div>
-            <div>divider</div>
-            <div>
-              <p>21 min</p>
-              <p>Delivery time</p>
+              Delivery fee
+              <span>tooltip</span>
             </div>
           </div>
+          <div>divider</div>
           <div>
-            Featured items
-            <div>Carousel</div>
+            <p>21 min</p>
+            <p>Delivery time</p>
           </div>
-          <div>
-            Reviews
-            <div>Review Carousel</div>
-          </div>
-          <hr />
-          <Text>Some text</Text>
+        </div>
+        <div>
+          Featured items
+          <div>Carousel</div>
+        </div>
+        <div>
+          Reviews
+          <div>Review Carousel</div>
+        </div>
+        <hr />
+        <Text>Some text</Text>
 
-          <ProductCard
-            image="https://via.placeholder.com/150"
-            title="Sausage, Cheddar & Egg Sandwich"
-            description="A savory sausage patty, fluffy cage-free eggs and aged Cheddar cheese on a perfectly toasted English muffin. -HIGH-PROTEIN"
-            price="$4.95"
-          />
-        </View>
-        <View backgroundColor="magenta-600" gridArea="footer" />
-      </Grid>
+        <ProductCard
+          image="https://via.placeholder.com/150"
+          title="Sausage, Cheddar & Egg Sandwich"
+          description="A savory sausage patty, fluffy cage-free eggs and aged Cheddar cheese on a perfectly toasted English muffin. -HIGH-PROTEIN"
+          price="$4.95"
+        />
+      </Layout>
     </Provider>
   );
 }
@@ -86,6 +74,29 @@ const ProductCard = ({ image, title, description, price }: any) => {
       <Text>{description}</Text>
       <Text>{price}</Text>
     </div>
+  );
+};
+
+const Layout = ({ children }: any) => {
+  return (
+    <Grid
+      areas={['header  header', 'sidebar content', 'footer  footer']}
+      // columns={['1fr', '3fr']}
+      columns={[240, '3fr']}
+      rows={['size-1000', 'auto', 'size-1000']}
+      height="100vh"
+      gap="size-100"
+    >
+      <View backgroundColor="celery-600" gridArea="header" />
+      <View backgroundColor="blue-600" gridArea="sidebar">
+        <ul>
+          <li>asldkjfasf</li>
+          <li>asldkfjdasf</li>
+        </ul>
+      </View>
+      <View gridArea="content">{children}</View>
+      <View backgroundColor="magenta-600" gridArea="footer" />
+    </Grid>
   );
 };
 
