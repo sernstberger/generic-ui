373847545e18e665f0256e6c247102d6d4d3d132
Steve Ernstberger <steve32285@gmail.com>
2024-04-03 10:07:15 -0400

    use icons

--- libs/components/src/lib/Carousel.tsx
+++ libs/components/src/lib/Carousel.tsx
@@ -1,3 +1,4 @@
+import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { ProductListItem, ProductListItemProps } from './ProductListItem';
 
 import { useState, useRef, useEffect } from 'react';
@@ -65,20 +66,7 @@ export const Carousel = ({ title, items }: CarouselProps) => {
             className="hover:bg-blue-900/75 text-white w-10 h-full text-center opacity-75 hover:opacity-100 disabled:opacity-25 disabled:cursor-not-allowed z-10 p-0 m-0 transition-all ease-in-out duration-300"
             disabled={isDisabled('prev')}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-12 w-20 -ml-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 19l-7-7 7-7"
-              />
-            </svg>
+            <ChevronLeft className="w-10 h-10" />
             <span className="sr-only">Prev</span>
           </button>
           <button
@@ -86,20 +74,7 @@ export const Carousel = ({ title, items }: CarouselProps) => {
             className="hover:bg-blue-900/75 text-white w-10 h-full text-center opacity-75 hover:opacity-100 disabled:opacity-25 disabled:cursor-not-allowed z-10 p-0 m-0 transition-all ease-in-out duration-300"
             disabled={isDisabled('next')}
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-12 w-20 -ml-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth={2}
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M9 5l7 7-7 7"
-              />
-            </svg>
+            <ChevronRight className="w-10 h-10" />
             <span className="sr-only">Next</span>
           </button>
         </div>
