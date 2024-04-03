7b49bf8552bff89cf1bb99017bd946005e0b90b2
Steve Ernstberger <steve32285@gmail.com>
2024-04-03 10:50:48 -0400

    first pass to simplify carousel item

--- libs/components/src/lib/Carousel.tsx
+++ libs/components/src/lib/Carousel.tsx
@@ -80,38 +80,46 @@ export const Carousel = ({ title, items }: CarouselProps) => {
           className="carousel-container relative flex gap-1 overflow-hidden scroll-smooth snap-x snap-mandatory touch-pan-x z-0"
         >
           {items.map((item, index) => {
-            return (
-              <div
-                key={index}
-                className="carousel-item text-center relative w-64 h-64 snap-start"
-              >
-                <a
-                  href="#"
-                  className="h-full w-full aspect-square block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0"
-                  style={{ backgroundImage: `url(${item.image || ''})` }}
-                >
-                  <img
-                    src={item.image || ''}
-                    alt={item.title}
-                    className="w-full aspect-square hidden"
-                  />
-                </a>
-                <a
-                  href="#"
-                  className="h-full w-full aspect-square block absolute top-0 left-0 transition-opacity duration-300 opacity-0 hover:opacity-100 bg-blue-800/75 z-10"
-                >
-                  <h3 className="text-white py-6 px-3 mx-auto text-xl">
-                    {item.title}
-                  </h3>
-                </a>
-              </div>
-            );
+            return <CarouselItem key={index} item={item} />;
           })}
         </div>
       </div>
     </div>
   );
 };
+
+interface CarouselItemProps {
+  item: {
+    image: string;
+    title: string;
+  };
+}
+
+const CarouselItem = ({ item }: CarouselItemProps) => (
+  <div className="carousel-item text-center relative w-64 h-64 snap-start">
+    {/* <a
+      href="#"
+      className="h-full w-full aspect-square block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0"
+      style={{ backgroundImage: `url(${item.image || ''})` }}
+    >
+      <img
+        src={item.image || ''}
+        alt={item.title}
+        className="w-full aspect-square hidden"
+      />
+    </a>
+    <a
+      href="#"
+      className="h-full w-full aspect-square block absolute top-0 left-0 transition-opacity duration-300 opacity-0 hover:opacity-100 bg-blue-800/75 z-10"
+    >
+      <h3 className="text-white py-6 px-3 mx-auto text-xl">{item.title}</h3>
+    </a> */}
+    <div className="h-full w-full aspect-square">
+      <img src={item.image || ''} alt={item.title} />
+      {item.title}
+    </div>
+  </div>
+);
 
 interface CarouselButtonProps {
   onClick: () => void;
